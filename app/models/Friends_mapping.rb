b4bbe04a1611b2647d9f72cfd2735b783cdce1ef
eyal engel <eyalengel@pagaya.com>
2020-06-06 07:49:34 +0300

    .

--- app/models/Friends_mapping.rb
+++ app/models/Friends_mapping.rb
@@ -19,6 +19,7 @@ class FriendsMapping < BaseDBModel
     def self.get_friends_mappings(owner_id)
         owner_id_obj = "ObjectId(%s)" % [owner_id]
         query = {'owner_id' => {"_id" =>  owner_id_obj}}
+        p query
         res = self.find(:Users, query).sort({ "priority": 1 })
         return res.to_json
     end 
