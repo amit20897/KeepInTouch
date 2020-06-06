35f29ba7e6376db100027fd39edfef6a8e147c0c
eyal engel <eyalengel@pagaya.com>
2020-06-06 06:19:20 +0300

    .

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,24 +1,24 @@
 require_relative "../models/base_db_model"
+require_relative "../models/user.rb"
 
 class FriendsController < ApplicationController
+
     def get_friend_list
-        db_connector = BaseDBModel.new()
-        query = {}
-        res = db_connector.find(:Users, query)
+        res = User.get_friend_list()
         render :json => res.to_json
     end
 
     def get_friend_by_username
-        db_connector = BaseDBModel.new()
-
         user_name = params.require(:username)
-        query = {'name' => user_name}
-        res = db_connector.find(:Users, query)
+        res = User.get_friend(user_name)
 	    render :json => res.to_json
     end
 
-    def add_friend
-        puts 33
+    def add_friend(friend_id, friend_name, priority)
+        User.add_friend(friend_id, friend_name)
+        res = FriendsMapping.create(@@id, friend_id, priority)
+        render :json => res.to_json
+    end
     end
 
 
