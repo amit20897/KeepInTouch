75457a821134d64702b2c7688b00652b7fd9ffbd
eyal engel <eyalengel@pagaya.com>
2020-06-06 06:08:59 +0300

    .

---
+++ app/models/Friends_mapping.rb
@@ -0,0 +1,39 @@
+class FriendsMapping < BaseDBModel
+    # @@_id = nil
+    @@friend_id = nil
+    @@owner_id = nil
+    @@priority = nil
+  
+  
+    #user_name:string id:string priority:string last_met:datetime last_talked:datetime address:string phone:string email:string
+    
+    def initialize(friend_id, owner_id, priority)
+      super
+      @friend_id = @friend_id
+      @priorities = @priorities
+    end
+    
+    def self.get_friends_mappings(owner_id)
+      query = {'owner_id' => owner_id}
+      res = self.find(:Users, query)
+      return res.to_json
+  
+    def self.create(owner_id, friend_id, priority)
+      doc = {
+          'owner_id' => owner_id,
+          'friend_id' => friend_id,
+          'priority' => priority
+      }
+      res = super(:FriendsMapping, doc)
+      return res.to_json
+    end
+  
+    def self.add_friend(friend_id, friend_name, )
+      doc = {
+        '_id' => _id,
+        'name' => name,
+        'priorities' => {}
+      }
+      self.create(:Users, doc)
+    end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,38 @@
-class User < ApplicationRecord
-  # attr_accessor :username, :plain_pass, :token, :id
-  attribute :username, :string
-  attribute :plain_pass, :string
-  attribute :token, :string
-  attribute :id, :string
+
+
+class User < BaseDBModel
+  # @@_id = nil
+  @@name = nil
+  priorities = nil
 
 
 
-end
+  #user_name:string id:string priority:string last_met:datetime last_talked:datetime address:string phone:string email:string
+  
+  def initialize(name, priorities)
+    super
+    # @_id = _id
+    @name = @name
+    @priorities = @priorities
+  end
+  
+  def self.get_friend(user_name)
+    query = {'name' => user_name}
+    res = self.find_one(:Users, query)
+    return res.to_json
 
+  def self.get_friends_list()
+    query = {}
+    res = self.find(:Users, query)
+    return res.to_json
+  end
 
+  def self.add_friend(friend_id, friend_name, )
+    doc = {
+      # '_id' => _id,
+      'name' => name,
+      'priorities' => {}
+    }
+    FriendsMapping.create()
+    self.create(:Users, doc)
+  end
