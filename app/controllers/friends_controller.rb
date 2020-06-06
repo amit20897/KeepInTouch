ada07adb13da182ca21d0c8b91055956c04c241d
eyal engel <eyalengel@pagaya.com>
2020-06-06 06:27:03 +0300

    .

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -20,29 +20,3 @@ class FriendsController < ApplicationController
         render :json => res.to_json
     end
     end
-
-
-
-
-
-
-
-
-    @user = User.find_by(username: params[:username])
-    if @user
-      puts  @user.to_json
-      render :json => @user.to_json
-    else
-      @token = Digest::MD5.hexdigest(params[:username]).to_s
-
-      user_hash = {
-        :username => params.require(:username),
-        :plain_pass => params.require(:plain_pass),
-        :token => @token
-      }
-      @user = User.new(user_hash)
-      puts "SAVED???"
-      puts @user.save
-
-      session[:user_id] = @user.id
-      render :json => @user.to_json
