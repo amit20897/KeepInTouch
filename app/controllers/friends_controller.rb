1c9280642a65dd3ad20d806e2e8ac9621b2ced2a
eyal engel <eyalengel@pagaya.com>
2020-06-06 07:31:55 +0300

    .

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,6 @@
 require_relative "../models/base_db_model"
 require_relative "../models/user.rb"
-require_relative "../models/friends_mapping.rb"
+require_relative "../models/Friends_mapping.rb"
 
 class FriendsController < ApplicationController
 
