b4b1596f6ccabb7c0775958e0f170ebb5b980ca9
Ubuntu <ubuntu@ip-172-31-32-223.us-east-2.compute.internal>
2020-06-06 02:51:46 +0000

    Basic suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'pages#home'
+
+get 'get_suggestion', to: 'suggest#get'
+
 end
