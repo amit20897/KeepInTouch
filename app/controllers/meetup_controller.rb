4cc1861a0d094d8297f8b08762fa421a0baf00f9
Tomer Shefi <shefitomer@gmail.com>
2020-06-06 07:55:54 +0000

    Finished basic check and submit

--- app/controllers/meetup_controller.rb
+++ app/controllers/meetup_controller.rb
@@ -6,20 +6,29 @@ class MeetupController < ApplicationController
 	  start_time = Time.parse(params[:start])
 	  end_time = Time.parse(params[:end])
 
-	  if not (start_time and end_time)
+	  if not (id and start_time and end_time)
 		  return
 	  end
 
 	  db = BaseDBModel.new()
 
-	  all_times = nil
+	  all_times = Array.new()
 	  db.find(:FriendsMapping, {:owner_id => BSON::ObjectId(params[:id])}).each do |friend|
 		  db.find(:Meetups, {:owner_id => friend[:friend_id]}).each do |doc|
 			  if time_fits(start_time, end_time, doc[:start], doc[:end])
-				  p doc
+				  user = db.find_one(:Users, {:_id => doc[:owner_id]})
+				  all_times << {
+							'start'		=> doc[:start],
+							'end'		=> doc[:end]  ,
+							'owner_id'	=> doc[:owner_id],
+							'name'		=> user[:name],
+							'meeting_id'	=> doc[:_id]
+				  }
 			  end
 		  end
 	  end
+
+	  render :json => all_times.compact
   end
 
   def time_fits(this_start, this_end, other_start, other_end)
@@ -36,5 +45,11 @@ class MeetupController < ApplicationController
   end
 
   def submit
+	  id = params[:id]
+	  start_time = params[:start]
+	  end_time = params[:start]
+	  db = BaseDBModel.new()
+
+	  db.create(:Meetups, {"owner_id": BSON::ObjectId(id), "start": Time.parse(start_time), "end": Time.parse(end_time)})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'meetup/check'
+  get 'meetup/submit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 root to: 'pages#home'
 
 get 'get_suggestion', to: 'suggest#get'
+
+post 'check_available', to: 'meetup#check', via: :post
 
 end
