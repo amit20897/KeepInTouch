96eec683071ae6e9ae0e6b564bfdf3335c7595eb
eyal engel <eyalengel@pagaya.com>
2020-06-06 08:36:07 +0300

    .

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,7 +6,10 @@ class FriendsController < ApplicationController
 
     def get_friends_list
         friends_mappings = FriendsMapping.get_friends_mappings('5edaa5f2ea613908a59465b7')
-        data_hash = JSON.parse(friends_mappings)        data_hash.each do |map|
+        p 'data_hash'
+        data_hash = JSON.parse(friends_mappings)
+        p data_hash
+        data_hash.each do |map|
             p map.friend_id
         end 
         res = User.get_users_list()
