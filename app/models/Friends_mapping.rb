8f4eb92959fb0de91fed106dfcd47efcdcfe1af1
eyal engel <eyalengel@pagaya.com>
2020-06-06 06:52:09 +0300

    .

--- app/models/Friends_mapping.rb
+++ app/models/Friends_mapping.rb
@@ -1,3 +1,82 @@
+require 'mongo'
+
+
+class FriendsMapping < BaseDBModel
+    @@_id = nil
+    @@friend_id = nil
+    @@owner_id = nil
+    @@priority = nil
+
+  #user_name:string id:string priority:string last_met:datetime last_talked:datetime address:string phone:string email:string
+  
+    def initialize(id, friend_id, owner_id, priority)
+        @@client = Mongo::Client.new('mongodb+srv://KeepingTouch2:NotmyName3@keepingtouch-cpcht.mongodb.net/KeepingTouch?retryWrites=true&w=majority')
+        @friend_id = @friend_id
+        @priorities = @priorities
+      end
+      
+      def self.get_friends_mappings(owner_id)
+        query = {'owner_id' => owner_id}
+        res = self.find(:Users, query)
+        return res.to_json
+    
+      def self.create(owner_id, friend_id, priority)
+        doc = {
+            'owner_id' => owner_id,
+            'friend_id' => friend_id,
+            'priority' => priority
+        }
+        res = super(:FriendsMapping, doc)
+        return res.to_json
+      end
+
+  def finalize(object_id)
+		@@client.close
+	end
+
+	def find(collection, filter)
+		collection = @@client[collection]
+		return collection.find(filter)
+	end
+
+	def find_one(collection, filter)
+		collection = @@client[collection]
+		return collection.find(filter).first
+	end
+
+	def create(collection, document)
+		collection = @@client[collection]
+		collection.insert_one(document)
+	end
+
+	def delete(collection, filter)
+		collection = @@client[collection]
+		result = collection.delete_may(filter)
+		return result.deleted_count
+	end
+
+	def update(collection, filter, changes)
+		collection = @@client[collection]
+		result = collection.update_one( filter, { '$set' => changes } )
+		p result.modified_count
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class FriendsMapping < BaseDBModel
     @@_id = nil
     @@friend_id = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
+require 'mongo'
 
 
-class User < BaseDBModel
-  # @@_id = nil
+class User 
+  @@client = nil
+  @@_id = nil
   @@name = nil
   priorities = nil
 
   #user_name:string id:string priority:string last_met:datetime last_talked:datetime address:string phone:string email:string
   
   def initialize(name, priorities)
-    super
+    @@client = Mongo::Client.new('mongodb+srv://KeepingTouch2:NotmyName3@keepingtouch-cpcht.mongodb.net/KeepingTouch?retryWrites=true&w=majority')
     # @_id = _id
     @name = @name
     @priorities = @priorities
@@ -22,7 +24,7 @@ class User < BaseDBModel
 
   def self.get_friends_list()
     query = {}
-    res = super.find(:Users, query)
+    res = find(:Users, query)
     return res.to_json
   end
 
@@ -35,4 +37,36 @@ class User < BaseDBModel
     FriendsMapping.create()
     self.create(:Users, doc)
   end
+
+  def finalize(object_id)
+		@@client.close
+	end
+
+	def find(collection, filter)
+		collection = @@client[collection]
+		return collection.find(filter)
+	end
+
+	def find_one(collection, filter)
+		collection = @@client[collection]
+		return collection.find(filter).first
+	end
+
+	def create(collection, document)
+		collection = @@client[collection]
+		collection.insert_one(document)
+	end
+
+	def delete(collection, filter)
+		collection = @@client[collection]
+		result = collection.delete_may(filter)
+		return result.deleted_count
+	end
+
+	def update(collection, filter, changes)
+		collection = @@client[collection]
+		result = collection.update_one( filter, { '$set' => changes } )
+		p result.modified_count
+	end
 end
+
